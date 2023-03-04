1c36a6e796fb8c1f323f40ea0afc93d1870100a1
Kevin Wittek <kiview@users.noreply.github.com>
2023-03-04 20:22:44 +0000

    Add first tests

---
+++ learn_by_tests/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return englishHelloPrefix + name + "."
+}
+
+func main() {
+	fmt.Println(Hello("world"))
+}
