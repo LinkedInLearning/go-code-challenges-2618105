4a4a9e040117a962a672879c68549a3919359e80
Kevin Wittek <kiview@users.noreply.github.com>
2023-03-11 14:16:48 +0000

    Add diamond

---
+++ challenges/diamond/diamond_test.go
@@ -0,0 +1,23 @@
+package diamond
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleDiamond() {
+	d := Diamond('A')
+	fmt.Println(d)
+	// Output:
+	//    A
+}
+
+func TestDiamond(t *testing.T) {
+	d := Diamond('D')
+
+	expectedDiamond := "   A   \n" + "  B B  \n" + " C   C \n" + "D     D\n C   C \n" + "  B B  \n" + "   A   "
+
+	if d != expectedDiamond {
+		t.Errorf("Expected: %s, got: %s", expectedDiamond, d)
+	}
+}
