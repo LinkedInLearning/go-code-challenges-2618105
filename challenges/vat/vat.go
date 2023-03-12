034af96028ff7a7d852674dfda2a94cb4828cef5
Kevin Wittek <kiview@users.noreply.github.com>
2023-03-12 09:55:36 +0000

    Better formatting in vat

--- challenges/vat/vat.go
+++ challenges/vat/vat.go
@@ -6,5 +6,6 @@ func RenderInvoice(price, vatRate float64) string {
 	vat := price * vatRate
 	total := price + vat
 
-	return fmt.Sprintf("Netto: %.2f €\nVAT (%.2f): %.2f €\nTotal: %.2f €", price, vatRate, vat, total)
+	return fmt.Sprintf("Netto: %.2f €\nVAT (%.2f): %.2f €\nTotal: %.2f €",
+		price, vatRate, vat, total)
 }
