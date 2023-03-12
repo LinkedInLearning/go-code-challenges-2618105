8d33aa1ef6fd31b667438da8fea9a509d8baf36b
Kevin Wittek <kiview@users.noreply.github.com>
2023-03-12 10:19:11 +0000

    Align example name for prime

--- challenges/prime/prime_test.go
+++ challenges/prime/prime_test.go
@@ -2,7 +2,7 @@ package prime
 
 import "fmt"
 
-func ExampleFindPrimes() {
+func ExampleFindPrimeNumbersInInterval() {
 	primes := FindPrimeNumbersInInterval(1, 10)
 	fmt.Println(primes)
 	// Output: [2 3 5 7]
