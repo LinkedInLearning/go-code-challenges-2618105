e588cf6f6a2f53a79b15a3d37de0f716cce81094
Kevin Wittek <kiview@users.noreply.github.com>
2023-03-12 10:36:21 +0000

    Better formatting of diamond test code

--- challenges/diamond/diamond_test.go
+++ challenges/diamond/diamond_test.go
@@ -15,7 +15,13 @@ func ExampleDiamond() {
 func TestDiamond(t *testing.T) {
 	d := Diamond('D')
 
-	expectedDiamond := "   A   \n" + "  B B  \n" + " C   C \n" + "D     D\n C   C \n" + "  B B  \n" + "   A   "
+	expectedDiamond := "   A   \n" +
+		"  B B  \n" +
+		" C   C \n" +
+		"D     D\n" +
+		" C   C \n" +
+		"  B B  \n" +
+		"   A   "
 
 	if d != expectedDiamond {
 		t.Errorf("Expected: %s, got: %s", expectedDiamond, d)
